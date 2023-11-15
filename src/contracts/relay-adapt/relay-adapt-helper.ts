4ba14068d4d87a070363bd2b8be690eaf5fc43b7
MuhammadWaqas4 <mwaqasbashir4@gmail.com>
2023-11-15 12:31:53 +0500

    engine

---
+++ src/contracts/relay-adapt/relay-adapt-helper.ts
@@ -0,0 +1,168 @@
+import { ContractTransaction, AbiCoder, keccak256 } from 'ethers';
+import { randomHex, hexToBytes } from '../../utils/bytes';
+import { ShieldNoteERC20 } from '../../note/erc20/shield-note-erc20';
+import { AddressData } from '../../key-derivation';
+import { NFTTokenData, TokenType } from '../../models/formatted-types';
+import { ShieldNoteNFT } from '../../note/nft/shield-note-nft';
+import { ERC721_NOTE_VALUE } from '../../note/note-util';
+import { RelayAdapt, ShieldRequestStruct, TransactionStruct } from '../../abi/typechain/RelayAdapt';
+
+class RelayAdaptHelper {
+  static async generateRelayShieldRequests(
+    addressData: AddressData,
+    random: string,
+    shieldERC20Addresses: string[],
+    shieldNFTsTokenData: NFTTokenData[],
+  ): Promise<ShieldRequestStruct[]> {
+    return Promise.all([
+      ...(await RelayAdaptHelper.createRelayShieldRequestsERC20s(
+        addressData,
+        random,
+        shieldERC20Addresses,
+      )),
+      ...(await RelayAdaptHelper.createRelayShieldRequestsNFTs(
+        addressData,
+        random,
+        shieldNFTsTokenData,
+      )),
+    ]);
+  }
+
+  private static async createRelayShieldRequestsERC20s(
+    addressData: AddressData,
+    random: string,
+    shieldERC20Addresses: string[],
+  ): Promise<ShieldRequestStruct[]> {
+    return Promise.all(
+      shieldERC20Addresses.map((erc20Address) => {
+        const shieldERC20 = new ShieldNoteERC20(
+          addressData.masterPublicKey,
+          random,
+          0n, // 0n will automatically shield entire balance.
+          erc20Address,
+        );
+
+        // Random private key for Relay Adapt shield.
+        const shieldPrivateKey = hexToBytes(randomHex(32));
+
+        return shieldERC20.serialize(shieldPrivateKey, addressData.viewingPublicKey);
+      }),
+    );
+  }
+
+  private static async createRelayShieldRequestsNFTs(
+    addressData: AddressData,
+    random: string,
+    shieldNFTsTokenData: NFTTokenData[],
+  ): Promise<ShieldRequestStruct[]> {
+    return Promise.all(
+      shieldNFTsTokenData.map((nftTokenData) => {
+        const value = RelayAdaptHelper.valueForNFTShield(nftTokenData);
+
+        const shieldERC20 = new ShieldNoteNFT(
+          addressData.masterPublicKey,
+          random,
+          value,
+          nftTokenData,
+        );
+
+        // Random private key for Relay Adapt shield.
+        const shieldPrivateKey = hexToBytes(randomHex(32));
+
+        return shieldERC20.serialize(shieldPrivateKey, addressData.viewingPublicKey);
+      }),
+    );
+  }
+
+  private static valueForNFTShield(nftTokenData: NFTTokenData): bigint {
+    switch (nftTokenData.tokenType) {
+      case TokenType.ERC721:
+        return ERC721_NOTE_VALUE;
+      case TokenType.ERC1155:
+        return 0n; // 0n will automatically shield entire balance.
+    }
+    throw new Error('Unhandled NFT token type.');
+  }
+
+  static validateShieldRequests(shieldRequests: ShieldRequestStruct[]) {
+    const { preimage } = shieldRequests[0];
+    shieldRequests.forEach((shieldInput) => {
+      if (shieldInput.preimage.npk !== preimage.npk) {
+        throw new Error('Relay shields must all contain the same npk/random.');
+      }
+    });
+  }
+
+  /**
+   * Format action data field for relay call.
+   */
+  static getActionData(
+    random: string,
+    requireSuccess: boolean,
+    calls: ContractTransaction[],
+    minGasLimit: bigint,
+  ): RelayAdapt.ActionDataStruct {
+    const formattedRandom = RelayAdaptHelper.formatRandom(random);
+    return {
+      random: formattedRandom,
+      requireSuccess,
+      minGasLimit,
+      calls: RelayAdaptHelper.formatCalls(calls),
+    };
+  }
+
+  /**
+   * Get relay adapt params hash.
+   * Hashes transaction data and params to ensure that transaction is not modified by MITM.
+   *
+   * @param transactions - serialized transactions
+   * @param random - random value
+   * @param requireSuccess - require success on calls
+   * @param calls - calls list
+   * @returns adapt params
+   */
+  static getRelayAdaptParams(
+    transactions: TransactionStruct[],
+    random: string,
+    requireSuccess: boolean,
+    calls: ContractTransaction[],
+    minGasLimit = BigInt(0),
+  ): string {
+    const nullifiers = transactions.map((transaction) => transaction.nullifiers);
+    const actionData = RelayAdaptHelper.getActionData(random, requireSuccess, calls, minGasLimit);
+
+    const preimage = AbiCoder.defaultAbiCoder().encode(
+      [
+        'bytes32[][] nullifiers',
+        'uint256 transactionsLength',
+        'tuple(bytes31 random, bool requireSuccess, uint256 minGasLimit, tuple(address to, bytes data, uint256 value)[] calls) actionData',
+      ],
+      [nullifiers, transactions.length, actionData],
+    );
+
+    return keccak256(hexToBytes(preimage));
+  }
+
+  /**
+   * Strips all unnecessary fields from populated transactions
+   *
+   * @param {object[]} calls - calls list
+   * @returns {object[]} formatted calls
+   */
+  static formatCalls(calls: ContractTransaction[]): RelayAdapt.CallStruct[] {
+    return calls.map((call) => ({
+      to: call.to || '',
+      data: call.data || '',
+      value: call.value || 0n,
+    }));
+  }
+
+  static formatRandom(random: string): Uint8Array {
+    if (random.length !== 62) {
+      throw new Error('Relay Adapt random parameter must be a hex string of length 62 (31 bytes).');
+    }
+    return hexToBytes(random);
+  }
+}
+
+export { RelayAdaptHelper };
---
+++ src/provider/polling-util.ts
@@ -0,0 +1,60 @@
+import { AbstractProvider, FallbackProvider, JsonRpcProvider } from 'ethers';
+import { PollingJsonRpcProvider } from './polling-json-rpc-provider';
+import { isDefined } from '../utils/is-defined';
+
+export const isNonPollingJsonRpcProvider = (provider: AbstractProvider) => {
+  if (!(provider instanceof JsonRpcProvider)) {
+    return false;
+  }
+  // eslint-disable-next-line no-underscore-dangle
+  return !isDefined(provider._getOption('polling')) || provider._getOption('polling') === false;
+};
+
+export const assertIsPollingProvider = (provider: AbstractProvider) => {
+  if (isNonPollingJsonRpcProvider(provider)) {
+    throw new Error(
+      'The JsonRpcProvider must have polling enabled. Use PollingJsonRpcProvider to instantiate.',
+    );
+  }
+};
+
+/**
+ * Fallback Providers don't poll correctly for events.
+ * This function creates a PollingJsonRpcProvider from the first provider in the FallbackProvider.
+ */
+export const createPollingJsonRpcProviderForListeners = async (
+  provider: JsonRpcProvider | FallbackProvider,
+  pollingInterval?: number,
+): Promise<PollingJsonRpcProvider> => {
+  if (provider instanceof PollingJsonRpcProvider) {
+    return provider;
+  }
+
+  if ('pollingInterval' in provider) {
+    // eslint-disable-next-line no-underscore-dangle
+    const { url } = provider._getConnection();
+    const { chainId } = await provider.getNetwork();
+    return new PollingJsonRpcProvider(url, Number(chainId), pollingInterval);
+  }
+
+  if ('quorum' in provider) {
+    // FallbackProvider only
+
+    if (!provider.providerConfigs.length) {
+      throw new Error('Requires 1+ providers in FallbackProvider');
+    }
+    const firstProvider = provider.providerConfigs[0].provider as JsonRpcProvider;
+    if (!('pollingInterval' in firstProvider)) {
+      throw new Error('First provider in FallbackProvider must be JsonRpcProvider');
+    }
+
+    // eslint-disable-next-line no-underscore-dangle
+    const { url } = firstProvider._getConnection();
+    const { chainId } = await provider.getNetwork();
+    // eslint-disable-next-line no-underscore-dangle
+    const maxLogsPerBatch = firstProvider._getOption('batchMaxCount');
+    return new PollingJsonRpcProvider(url, Number(chainId), pollingInterval, maxLogsPerBatch);
+  }
+
+  throw new Error('Invalid ethers provider type');
+};
